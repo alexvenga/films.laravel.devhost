fe87f1b9ed77470418c7a711419b1e22914235f9
alexvenga <bubnov.alexey@gmail.com>
2022-06-12 01:19:23 +0300

    Cache img

--- public/egg.php
+++ public/egg.php
@@ -5,6 +5,11 @@
     <title>Title</title>
 </head>
 <body style="display: flex; justify-content: center; align-items: center; width: 100%; height: 100vh;">
-<a href="/egg.html"><img src="/assets/img/qr-code.gif" width="570" height="570" border="0" title="QR код"></a>
+<div style="text-align: center;">
+    <a href="/egg.html"><img src="/assets/img/qr-code.gif" width="570" height="570" border="0" title="QR код"></a>
+    <div>
+        3062d4f3570eee39b4988c7fd868df355a119e5525267be0829a2d51780b342af5425e71f499f18c8247f5e7ebf55e3d
+    </div>
+</div>
 </body>
 </html>
