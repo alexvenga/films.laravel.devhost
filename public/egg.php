31f15d476b5649336444096f9a10548a1ae244eb
alexvenga <bubnov.alexey@gmail.com>
2022-06-12 01:13:45 +0300

    Cache img

--- public/egg.php
+++ public/egg.php
@@ -5,18 +5,6 @@
     <title>Title</title>
 </head>
 <body style="display: flex; justify-content: center; align-items: center; width: 100%; height: 100vh;">
-<div style="width: 1000px; height: 500px; display: flex; flex-wrap: wrap; overflow: hidden; position: relative;">
-    <?php for ($i = 1; $i <= 1000; $i++) {
-        $r = mt_rand(175, 255);
-        $g = mt_rand(175, 255);
-        $b = mt_rand(175, 255);
-        ?>
-        <div style="background-color: rgb(<?php echo $r; ?>,<?php echo $g; ?>,<?php echo $b; ?>); display: block; flex-grow: 1;"
-        ><?php echo substr(md5(mt_rand()), 0, 2); ?></div>
-    <?php } ?>
-    <a href="#" style="display: block; position: absolute; width: 1000px; height: 500px; top:0; right: 0; background-image: url('assets/img/noize-gradient.png');">
-
-    </a>
-</div>
+<a href="/egg.html"><img src="http://qrcoder.ru/code/?840ee0a9f4deb2ca30714b1b518aee33954a2a468281e9049ef3e3fa23112f5cc0298c396878a2b92a5145094eca605afd195b58c771c5c19a6ff6ec5b738948&10&0" width="570" height="570" border="0" title="QR код"></a>
 </body>
 </html>
