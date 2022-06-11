a23c91936e79607570b6d0758e397877f81cfc31
alexvenga <bubnov.alexey@gmail.com>
2022-06-12 00:41:20 +0300

    Cache img

--- public/egg.php
+++ public/egg.php
@@ -5,16 +5,18 @@
     <title>Title</title>
 </head>
 <body style="display: flex; justify-content: center; align-items: center; width: 100%; height: 100vh;">
-<a href="/egg.html" style="width: 1000px; height: 500px; display: flex; flex-wrap: wrap; overflow: hidden;">
+<div style="width: 1000px; height: 500px; display: flex; flex-wrap: wrap; overflow: hidden; position: relative;">
     <?php for ($i = 1; $i <= 1000; $i++) {
-        $r = mt_rand(150,255);
-        $g = mt_rand(150,255);
-        $b = mt_rand(150,255);
+        $r = mt_rand(200, 255);
+        $g = mt_rand(200, 255);
+        $b = mt_rand(200, 255);
         ?>
-        <div
-            style="background-color: rgb(<?php echo $r; ?>,<?php echo $g; ?>,<?php echo $b; ?>); display: block; flex-grow: 1;"
+        <div style="background-color: rgb(<?php echo $r; ?>,<?php echo $r; ?>,<?php echo $r; ?>); display: block; flex-grow: 1;"
         ><?php echo substr(md5(mt_rand()), 0, 2); ?></div>
     <?php } ?>
-</a>
+    <a href="#" style="display: block; position: absolute; width: 1000px; height: 500px; top:0; right: 0; background-image: url('assets/img/noize-gradient.png');">
+
+    </a>
+</div>
 </body>
 </html>
